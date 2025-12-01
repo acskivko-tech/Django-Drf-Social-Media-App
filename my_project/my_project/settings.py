cfd9003598949694df2a1dca80a7cccd28c33710
a.c <acskivko@gmail.com>
2025-12-01 14:22:22 +0200

    Create URLs for all apps, add Swagger and Redoc schema routes, and configure DRF Spectacular in settings

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -126,3 +126,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
