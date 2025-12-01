218c029ddb6521afb43f2b9a419e18baca9ce2b0
a.c <acskivko@gmail.com>
2025-12-01 15:07:39 +0200

    Create CustomUser model and BlogMessage model with author and genre relationships

--- my_project/core/models.py
+++ my_project/core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+from users.models import CustomUser
+
+
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class BlogMessageModel(models.Model):
+    title = models.CharField(
+        max_length=100
+    )
+
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name='posts')
+
+    content = models.TextField(
+        blank=True,
+    )
+
+    date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='messages')
--- my_project/users/models.py
+++ my_project/users/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
+
+
+class Genders(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Country(models.Model):
+    country_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.country_name
+
+class CustomUser(AbstractUser):
+    gender = models.ForeignKey(
+        Genders,
+        on_delete=models.PROTECT,
+        null=True,
+        related_name='users',
+        default=None)
+
+    age = models.IntegerField(
+        default=18,
+        validators=[MinValueValidator(18), MaxValueValidator(150)],
+    )
+
+    country_born = models.ForeignKey(
+        Country, on_delete=models.PROTECT,
+        null=True,
+        related_name='users',
+        default=None)
+
+    def __str__(self):
+        return self.username
