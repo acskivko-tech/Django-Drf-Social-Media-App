a7bb57d269c0cb0f4f4b2f9939e2d3cebc99eb24
a.c <acskivko@gmail.com>
2025-12-08 11:59:56 +0200

    Add base.html template in Core templates directory

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'my_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'core/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
