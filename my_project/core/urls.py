d48e06d9309233e1caaaca5b89b85a23f4ce5af7
a.c <acskivko@gmail.com>
2025-12-08 12:20:16 +0200

    Add MainPageView, path to core/urls, and welcome.html

--- my_project/core/urls.py
+++ my_project/core/urls.py
@@ -1,5 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
 
+from core.views import MainPageView
+
 urlpatterns = [
+    path('', MainPageView.as_view(), name='main'),
 ]
--- my_project/core/views.py
+++ my_project/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+"""Render main page depends on authentication of user """
+class MainPageView(TemplateView):
+    """Func to check if user is logged in"""
+    def get_template_names(self):
+        if self.request.user.is_authenticated:
+            return ['base.html']
+        else:
+            return ['welcome.html']
+
