d3ec735d3388b4d1951bf397916dc43b4fbcee96
a.c <acskivko@gmail.com>
2025-12-08 15:16:11 +0200

    Add login and register forms in users/forms.py, create LoginView and RegisterView

---
+++ my_project/users/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import forms
+
+from users.models import CustomUser
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email','first_name','last_name','gender','age','country_born','password1','password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already registered.")
+        return email
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
--- my_project/users/views.py
+++ my_project/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from djoser.webauthn.views import LoginView
+
+from users.forms import RegisterForm, LoginForm
+
 
 # Create your views here.
+
+class RegisterUserView(CreateView):
+    template_name = 'users/register.html'
+    form_class = RegisterForm
+    success_url = reverse_lazy('login')
+
+
+class LoginUserView(LoginView):
+    template_name = 'users/login.html'
+    form_class = LoginForm
+    success_url = reverse_lazy('welcome')
